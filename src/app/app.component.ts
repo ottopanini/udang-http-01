3c4521802614128ffba52b5ddea07ac921d1fdb4
cpress <cpress@it4ipm.de>
2020-05-30 12:53:15 +0200

    Using RxJS Operators to Transform Response Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -33,10 +34,20 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
-    this.http.get(AppComponent.POSTS_URL)
+    const subscription = this.http.get(AppComponent.POSTS_URL)
+      .pipe(map(responseData => {
+        const postsArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({ ...responseData[key], id: key });
+          }
+        }
+
+        return postsArray;
+      }))
       .subscribe(posts => {
         console.log(posts);
-    });
+      });
   }
 
   onClearPosts() {
