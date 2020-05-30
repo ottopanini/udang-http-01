e2b57a516b3a05bf01ffb82eaff25c5c5fd694d4
cpress <cpress@it4ipm.de>
2020-05-30 13:24:45 +0200

    Using a Service for Http Requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import {Post} from './post.model';
+import {PostsService} from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -9,50 +10,21 @@ import {Post} from './post.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  private static readonly BASE_URL = 'https://udang-http-requests.firebaseio.com';
-  private static readonly POSTS_URL = AppComponent.BASE_URL + '/posts.json';
   loadedPosts: Post[] = [];
   isFetching = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.postsService.fetchPosts();
   }
 
   onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    this.http
-      .post<{name: string}>(
-        AppComponent.POSTS_URL,
-        postData
-      )
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
-    this.fetchPosts();
-  }
-
-  private fetchPosts() {
-    this.isFetching = true;
-    this.http.get<{ [key: string]: Post }>(AppComponent.POSTS_URL)
-      .pipe(map(responseData => {
-        const postsArray: Post[] = [];
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            postsArray.push({ ...responseData[key], id: key });
-          }
-        }
-
-        return postsArray;
-      }))
-      .subscribe(posts => {
-        this.loadedPosts = posts;
-        this.isFetching = false;
-      });
+    this.postsService.fetchPosts();
   }
 
   onClearPosts() {
---
+++ src/app/posts.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Post} from './post.model';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+  private static readonly BASE_URL = 'https://udang-http-requests.firebaseio.com';
+  public static readonly POSTS_URL = PostsService.BASE_URL + '/posts.json';
+
+  constructor(private http: HttpClient) {}
+
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = {title, content};
+    this.http
+      .post<{name: string}>(
+        PostsService.POSTS_URL,
+        postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
+
+  fetchPosts() {
+    this.http.get<{ [key: string]: Post }>(PostsService.POSTS_URL)
+      .pipe(map(responseData => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({ ...responseData[key], id: key });
+          }
+        }
+
+        return postsArray;
+      }))
+      .subscribe(posts => {
+    });
+  }
+
+}
